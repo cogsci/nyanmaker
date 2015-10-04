b0620dc880fd4925bcc8b335f469765de75c6a0a
Eric Pan <ericpanbot@gmail.com>
2015-10-04 15:45:21 -0700

    Fly right offscreen on click

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('html').click(function() {
+    $('.content').animate({
+      marginRight: '-50%',
+      opacity: 0.25
+    }, 500, function() {
+      // Animation complete
+    });
+  });
+});
