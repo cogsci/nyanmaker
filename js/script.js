baef2ebe0a6c536355f1f512de4349265902c304
Eric Pan <ericpanbot@gmail.com>
2015-10-04 22:44:44 -0700

    Loop cat to reappear at start point

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-$(document).ready(function() {
-  $('.content').velocity({
-    translateX: "1050px"
-  }, 500);
+function appear() {
+  $('.content').velocity({translateX: '-1000px'}, {duration: 1, display: 'hidden'});
+  $('.content').velocity({translateX: '100px'}, {duration: 500, display: 'visible'});
+}
+$(function() {
+  appear();
   $('html').click(function() {
     $('.content').velocity({
-      translateX: "5000px"
-    }, 500, function() {
-      // Animation complete
+      // properties and values
+      translateX: '5000px'
+    }, {
+      // options
+      duration: 500,
+      complete: function() {
+        appear();
+      }
     });
   });
 });
