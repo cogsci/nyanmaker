1b1c046413e63baa2b7e48a96a1de57f9a251c3d
Eric Pan <ericpanbot@gmail.com>
2015-10-05 02:11:35 -0700

    Autoplay nyan song and keyboard controls

--- js/script.js
+++ js/script.js
@@ -6,8 +6,22 @@ function appear() {
 $(function() {
   appear();
 
-  var trips = 0;
+  // controls audio playback
+  Mousetrap.bind('a', function() {
+    document.getElementById('nyansong').play();
+  });
+  Mousetrap.bind('m', function() {
+    document.getElementById('nyansong').pause();
+  });
+  Mousetrap.bind('up', function() {
+    document.getElementById('nyansong').volume+=0.1;
+  });
+  Mousetrap.bind('down', function() {
+    document.getElementById('nyansong').volume-=0.1;
+  });
 
+  // displays number of trips cat has taken
+  var trips = 0;
   function incrementTrips() {
     trips++;
     $('p').velocity({
