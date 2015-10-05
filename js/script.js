8caed32c6979c313a65562163d249ee90c51711f
Eric Pan <ericpanbot@gmail.com>
2015-10-05 01:40:20 -0700

    Add trip counter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
+// makes cat appear from the left
 function appear() {
   $('.cat').velocity({translateX: '-1000px'}, {duration: 1, display: 'hidden'});
   $('.cat').velocity({translateX: '100px'}, {duration: 500, display: 'visible'});
 }
 $(function() {
   appear();
+
+  var trips = 0;
+
+  function incrementTrips() {
+    trips++;
+    $('p').velocity({
+      opacity: 1
+    },{
+      duration: 2000
+    });
+    $('.counter')[0].textContent = trips;
+    console.log(trips);
+  }
+
   $('html').click(function() {
+    // makes cat fly away to the right then return from left
     $('.cat').velocity({
       // properties and values
       translateX: '5000px'
@@ -13,6 +29,7 @@ $(function() {
       duration: 500,
       complete: function() {
         appear();
+        incrementTrips();
       }
     });
   });
