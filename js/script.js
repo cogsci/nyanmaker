5a685ba74b962f073fcd9d9cd3cef895d39a9b77
Eric Pan <ericpanbot@gmail.com>
2015-10-04 18:28:17 -0700

    Animate in before waiting for click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+  $('.content').velocity({
+    translateX: "1050px"
+  }, 500);
   $('html').click(function() {
     $('.content').velocity({
-      translateX: "2000px"
+      translateX: "5000px"
     }, 500, function() {
       // Animation complete
     });
