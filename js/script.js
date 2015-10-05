a5cfed2603d6a8de652c6163425fea67e43f0a93
Eric Pan <ericpanbot@gmail.com>
2015-10-04 22:50:16 -0700

    Use .cat instead of .content

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function appear() {
-  $('.content').velocity({translateX: '-1000px'}, {duration: 1, display: 'hidden'});
-  $('.content').velocity({translateX: '100px'}, {duration: 500, display: 'visible'});
+  $('.cat').velocity({translateX: '-1000px'}, {duration: 1, display: 'hidden'});
+  $('.cat').velocity({translateX: '100px'}, {duration: 500, display: 'visible'});
 }
 $(function() {
   appear();
   $('html').click(function() {
-    $('.content').velocity({
+    $('.cat').velocity({
       // properties and values
       translateX: '5000px'
     }, {
